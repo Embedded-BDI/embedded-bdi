ffe89e716ba7b39b58fc99c64a5488e78870bfa2
Mat Muller <matuzalemmuller@gmail.com>
2020-07-06 22:26:02 -0300

    Add cmake file for agent_loop
    * Add build folder to gitignore
    * Change Eclipse config for src project to link relative lib path

--- src/agent_loop.cpp
+++ src/agent_loop.cpp
@@ -6,10 +6,10 @@
  */
 
 #include <iostream>
-#include "bdi/belief_base.h"
-#include "bdi/event_base.h"
-#include "bdi/plan_base.h"
-#include "bdi/intention_base.h"
+#include "../lib/bdi/belief_base.h"
+#include "../lib/bdi/event_base.h"
+#include "../lib/bdi/plan_base.h"
+#include "../lib/bdi/intention_base.h"
 
 using namespace std;
 
@@ -56,47 +56,3 @@ int main()
 
   return 0;
 }
-
-
-
-//// ---- Alternate main function using new allocator
-//int main() {
-//  int bb_size = 4;
-//  int eb_size = 4;
-//  int pb_size = 4;
-//  int ib_queue_size = 4;
-//  int ib_stack_size = 2;
-//
-//  BeliefBase * beliefs = new BeliefBase(bb_size);
-//  EventBase * events = new EventBase(eb_size);
-//  PlanBase * plans = new PlanBase(pb_size, beliefs);
-//  IntentionBase * intentions = new IntentionBase(ib_queue_size, ib_stack_size, beliefs, events);
-//
-//  Event * event_to_process;
-//  Plan * plan_to_act;
-//
-//  while(true)
-//  {
-//    beliefs->update(events);
-//    if (!events->is_empty())
-//    {
-//      event_to_process = events->get_event();
-//      if(event_to_process)
-//      {
-//        plan_to_act = plans->revise(event_to_process);
-//        if (plan_to_act) {
-//          intentions->add_intention(plan_to_act);
-//        }
-//      }
-//    }
-//    intentions->run_intention_base();
-//
-//    if (compile_test == true)
-//    {
-//      std::cout << "Compilation successful!" << std::endl;
-//      break;
-//    }
-//  }
-//
-//  return 0;
-//}
